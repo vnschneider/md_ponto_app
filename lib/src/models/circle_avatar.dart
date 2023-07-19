bfe595bb2ae81087e5e5aa00ad8fe0809ba04af6
Vinícius Schneider <mrschneider.v@gmail.com>
2023-07-19 05:57:16 -0300

    add circleAvatar(normal and small) models

--- lib/src/models/circle_avatar.dart
+++ lib/src/models/circle_avatar.dart
@@ -1,7 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+
+class ProfileCircleAvatar{
+  final BuildContext context;
+  final double radius;
+  final int image;
+
+  ProfileCircleAvatar({
+    required this.context,
+    required this.radius,
+    required this.image,
+  });
+
+  Widget normal() {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+              color: Theme.of(context).colorScheme.background, width: 4),
+          borderRadius: BorderRadius.circular(100)),
+      // alignment: Alignment.center,
+
+      child: CircleAvatar(
+        radius: radius,
+        backgroundColor: Theme.of(context).colorScheme.secondary.withOpacity(0.6),
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: SvgPicture.asset(
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            semanticsLabel: 'Avatar',
+            'assets/avatars/avatar$image.svg',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget small(){
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+              color: Theme.of(context).colorScheme.background, width: 1),
+          borderRadius: BorderRadius.circular(100)),
+      // alignment: Alignment.center,
+
+      child: CircleAvatar(
+        radius: radius,
+        backgroundColor: Theme.of(context).colorScheme.secondary.withOpacity(0.6),
+        child: Padding(
+          padding: const EdgeInsets.all(0.5),
+          child: SvgPicture.asset(
+            alignment: Alignment.center,
+            fit: BoxFit.contain,
+            semanticsLabel: 'Avatar',
+            'assets/avatars/avatar$image.svg',
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
 profileCircleAvatar(context, radius, image) {
+  
   return Container(
     decoration: BoxDecoration(
         border: Border.all(
@@ -24,3 +87,4 @@ profileCircleAvatar(context, radius, image) {
     ),
   );
 }
+
