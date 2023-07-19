d1608c8e994049581b9836695eb9b292ea7b2c7a
Vinícius Schneider <mrschneider.v@gmail.com>
2023-07-19 05:57:52 -0300

    update lightTheme

--- lib/src/utils/theme.dart
+++ lib/src/utils/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 //crie o lightTheme com as cores 6D62D5 como primaria, FFFFFF como background e 343A40 como neutral e textos
@@ -7,6 +8,27 @@ ThemeData lightTheme() {
     useMaterial3: true,
     brightness: Brightness.light,
 
+    appBarTheme: AppBarTheme(
+      backgroundColor: const Color(0xFF6D62D5),
+      titleTextStyle: GoogleFonts.poppins(
+        fontSize: 18,
+        fontWeight: FontWeight.w500,
+        color: const Color(0xFFFFFFFF),
+      ),
+      iconTheme: const IconThemeData(
+        color: Color(0xFFFFFFFF),
+      ),
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        // Status bar color
+        statusBarColor: Color(0xFF6D62D5),
+        statusBarIconBrightness: Brightness.light,
+        statusBarBrightness: Brightness.dark,
+      ),
+    ),
+    navigationBarTheme: const NavigationBarThemeData(
+      backgroundColor: Color(0xFFFFFFFF),
+      indicatorColor: Color(0xFF6D62D5),
+    ),
     colorScheme: const ColorScheme.light(
       primary: Color(0xFF6D62D5),
       secondary: Color(0xFFECEBFA),
@@ -15,6 +37,10 @@ ThemeData lightTheme() {
       surface: Color(0xCC343A40),
       surfaceVariant: Color(0xFFD4D4D8),
       onSurface: Color(0xFFFFFFFF),
+      //custom color scheme
+      error: Color(0xFFF6533D),
+      onPrimary: Color(0xFFFFCD34),
+      onSecondary: Color(0xFF31C21E),
     ),
 
     scaffoldBackgroundColor: const Color(0xFF6D62D5),
@@ -74,19 +100,19 @@ ThemeData lightTheme() {
       ),
       bodyMedium: GoogleFonts.poppins(
         fontSize: 14,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w400,
         color: const Color(0xCC343A40),
       ),
       bodySmall: GoogleFonts.poppins(
         fontSize: 12,
-        fontWeight: FontWeight.w500,
+        fontWeight: FontWeight.w300,
         color: const Color(0xCC343A40),
       ),
     ),
   );
 }
 
-//dark them com as cores 6D62D5 como primaria
+//dark theme com as cores 6D62D5 como primaria
 ThemeData darkTheme() {
   return ThemeData(
     useMaterial3: true,
